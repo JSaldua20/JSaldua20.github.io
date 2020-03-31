478dd8e2fc1b64352b4d664e2ab8c87fcebe18c4
Ubuntu <jsaldua@tamu.edu>
2020-03-31 19:44:28 +0000

    This succesful commit shows the code for ruby

---
+++ ruby/code.rb
@@ -0,0 +1,15 @@
+# Program to print elements of an array
+# Save this as printarray.rb
+
+array1 = ["Apple", "Banana", "Orange", "Strawberry", "Watermelon"]
+puts "These are all different types of my favorite fruit:  "
+puts " "
+for array_element in array1
+          puts array_element
+end
+TIME = [Time.now]
+puts " "
+puts "The current time is: "
+puts TIME
+puts " "
+
